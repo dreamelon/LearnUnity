a15e26b08aba3bf533bd995585d23f066d0deb04
SNDA\wangshihao <shihao7679@qq.com>
2019-11-14 21:39:52 +0800

    手写渲染管线的延迟渲染

--- Assets/Scripts/SRPCamera.cs
+++ Assets/Scripts/SRPCamera.cs
@@ -19,7 +19,11 @@ public class SRPCamera : MonoBehaviour
     //public DeferredLighting lighting;
     public RenderTexture depthTexture;
 
-
+    public Material deferredMaterial;
+    public Light directionLight;
+    private static int _InvVP = Shader.PropertyToID("_InvVP");
+    private static int _CurrentLightDir = Shader.PropertyToID("_CurrentLightDir");
+    private static int _LightFinalColor = Shader.PropertyToID("_LightFinalColor");
     // Start is called before the first frame update
     void Start()
     {
@@ -31,12 +35,14 @@ public class SRPCamera : MonoBehaviour
             new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Linear),
             new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Linear),
         };
+        //深度贴图？
         depthTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Depth, RenderTextureReadWrite.Linear);
         GBuffers = new RenderBuffer[GBufferTextures.Length];
         for(int i = 0; i < GBuffers.Length; i++)
         {
             GBuffers[i] = GBufferTextures[i].colorBuffer;
         }
+        //这就是纹理id把
         GBufferIDs = new int[]
         {
             Shader.PropertyToID("_GBuffer0"),
@@ -50,20 +56,36 @@ public class SRPCamera : MonoBehaviour
     {
         Camera cam = Camera.current;
         Shader.SetGlobalTexture(_DepthTexture, depthTexture);
+        //渲染目标改为gbuffer
         Graphics.SetRenderTarget(GBuffers, depthTexture.depthBuffer);
-        Graphics.SetRenderTarget(cameraRT);
+        //Graphics.SetRenderTarget(cameraRT);
         GL.Clear(true, true, Color.grey);
         //start dc
-        pureColorMaterial.color = new Color(0, 0.5f, 0.8f);
+        pureColorMaterial.color = new Color(0, 0.2f, 0.8f);
         pureColorMaterial.SetPass(0);
         foreach (var i in cubeTrans)
         {
-            Graphics.DrawMeshNow(cubeMesh, Matrix4x4.identity);
+            Graphics.DrawMeshNow(cubeMesh, i.localToWorldMatrix);
         }
+
+        DrawLight(GBufferTextures, GBufferIDs, cameraRT, cam);
 
         skybox.DrawSkybox(cam, cameraRT);
         //end dc
         Graphics.Blit(cameraRT, cam.targetTexture);
+    }
+    public void DrawLight(RenderTexture[] gbuffers, int[] gbufferIDs, RenderTexture target, Camera cam)
+    {
+        Matrix4x4 proj = GL.GetGPUProjectionMatrix(cam.projectionMatrix, false);
+        Matrix4x4 vp = proj * cam.worldToCameraMatrix;
+        deferredMaterial.SetMatrix(_InvVP, vp.inverse);
+        deferredMaterial.SetVector(_CurrentLightDir, directionLight.transform.forward);
+        deferredMaterial.SetVector(_LightFinalColor, directionLight.color * directionLight.intensity);
+        for(int i=0; i < gbufferIDs.Length; ++i)
+        {
+            deferredMaterial.SetTexture(gbufferIDs[i], gbuffers[i]);
+        }
+        Graphics.Blit(null, target, deferredMaterial, 0);
     }
 }
 
