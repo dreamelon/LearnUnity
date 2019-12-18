af0a647b8ee952c70bc49ce63ef47aaf69167030
SNDA\wangshihao <shihao7679@qq.com>
2019-12-18 19:54:21 +0800

    运动模糊

---
+++ Assets/Scripts/MotionBlur.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 运动模糊通常有两种方式累积缓存和速度缓存
+/// 本节依靠将当前渲染结果叠加到之前存储的渲染结果中，达到运动模糊的效果
+/// </summary>
+public class MotionBlur : PostProcess
+{
+    [Range(0.0f, 0.9f)]
+    public float blurAmount = 0.5f;
+
+    private RenderTexture accumulationTexture;
+
+    private void OnDisable()
+    {
+        Destroy(accumulationTexture);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (_Material != null)
+        {
+            if (!accumulationTexture || accumulationTexture.width != source.width || accumulationTexture.height != source.height)
+            {
+                Destroy(accumulationTexture);
+                accumulationTexture = new RenderTexture(source.width, source.height, 0);
+                //意味着该变量不会显示在hierarchy中，也不会保存在scene中，就无法被使用者修改
+                accumulationTexture.hideFlags = HideFlags.HideAndDontSave;
+                Graphics.Blit(source, accumulationTexture);
+
+            }
+            //恢复操作
+            accumulationTexture.MarkRestoreExpected();
+
+            _Material.SetFloat("_BlurAmount", 1.0f - blurAmount);
+            Graphics.Blit(source, accumulationTexture, _Material);
+            Graphics.Blit(accumulationTexture, destination);
+
+        }
+        else
+            Graphics.Blit(source, destination);
+    }
+
+
+}
---
+++ Assets/Shaders/motionBlur.shader
@@ -0,0 +1,84 @@
+﻿Shader "Custom/motionBlur"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		_BlurAmount("Blur Amount", Float) = 1.0
+	}
+		SubShader
+		{
+			CGINCLUE
+			#include "UnityCG.cginc"
+				sampler2D _MainTex;
+				fixed _BlurAmount;
+				struct appdata
+				{
+					float4 vertex : POSITION;
+					float2 uv : TEXCOORD0;
+				};
+
+				struct v2f
+				{
+					float2 uv : TEXCOORD0;
+					UNITY_FOG_COORDS(1)
+						float4 vertex : SV_POSITION;
+				};
+
+
+				v2f vert(appdata v)
+				{
+					v2f o;
+					o.vertex = UnityObjectToClipPos(v.vertex);
+					o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+					UNITY_TRANSFER_FOG(o, o.vertex);
+					return o;
+				}
+
+				fixed4 fragRGB(v2f i) : SV_Target
+				{
+					// sample the texture
+					fixed4 col = fixed4(tex2D(_MainTex, i.uv).rgb, _BlurAmount);
+
+					return col;
+				}
+
+				half4 fragA(v2f i) : SV_Target{
+					return tex2D(_MainTex, i.uv);
+				}
+
+			ENDCG
+
+
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+		ZTest Always
+		Cull Off
+		ZWrite Off
+
+        Pass
+        {
+			Blend SrcAlpha OneMinusSrcAlpha
+			ColorMask RGB
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment fragRGB
+            ENDCG
+        }
+		Pass{
+			Blend One Zero
+			ColorMask A
+
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment fragA
+
+			ENDCG
+
+		}
+    }
+
+	Fallback Off
+}
